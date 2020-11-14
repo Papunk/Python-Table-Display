2fae6166025a4ce6661f4adbd6de830e4a3d8ce6
Pedro <pedrocpt2001@gmail.com>
2020-11-13 22:20:29 -0400

    added backbone classes

---
+++ Table.py
@@ -0,0 +1,18 @@
+class Cell:
+
+    def __init__(self, contents):
+        self.contents = contents
+
+
+class Row:
+
+    def __init__(self, length = 0):
+        self.length = length
+    
+
+class Table:
+
+    def __init__(self):
+        self.table = []
+
+
